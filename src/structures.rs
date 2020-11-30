875db313874ceb24e8ddbdde54416578d1699124
Manoj Karthick Selva Kumar <manojkarthick@ymail.com>
2020-11-29 22:19:58 -0800

    Move all structs into a single module

--- src/structures.rs
+++ src/structures.rs
@@ -1,9 +1,51 @@
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use serde_json::Value;
+
+/// Data structure that represents a user's info
+#[derive(Debug, Serialize, Deserialize)]
+pub struct AboutData {
+    /// Comment karma of the user
+    pub comment_karma: i64,
+    /// The time the user was created in seconds
+    pub created: f64,
+    /// The time the user was created in seconds (UTC)
+    pub created_utc: f64,
+    /// Undocumented
+    pub has_subscribed: bool,
+    /// Whether the user has verified their email
+    pub has_verified_email: bool,
+    /// Don't know
+    pub hide_from_robots: bool,
+    /// The id of the user
+    pub id: String,
+    /// Whether the user is a Reddit employee
+    pub is_employee: bool,
+    /// Whether the user is friend of the current user
+    pub is_friend: bool,
+    /// Whether the user has Reddit gold or not
+    pub is_gold: bool,
+    /// Whether the user is a moderator
+    pub is_mod: bool,
+    /// Link karma of the user
+    pub link_karma: i64,
+    /// The user's username
+    pub name: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct UserAbout {
+    /// The kind of object this is. eg: Comment, Account, Subreddit, etc.
+    pub kind: String,
+    /// Contains data about the reddit user
+    pub data: AboutData,
+}
 
 #[derive(Deserialize, Debug)]
 pub struct UserSaved {
+    /// The kind of object this is. eg: Comment, Account, Subreddit, etc.
     pub kind: String,
+    /// Contains the data for the children of the listing.
+    /// Listings are collections of data. For example, saved posts, hot posts in a subreddit
     pub data: ListingData,
 }
 
@@ -21,7 +63,9 @@ pub struct ListingData {
 
 #[derive(Deserialize, Debug, Clone)]
 pub struct Post {
+    /// The kind of object this is. eg: Comment, Account, Subreddit, etc.
     pub kind: String,
+    /// Contains data about this particular reddit post
     pub data: PostData,
 }
 
