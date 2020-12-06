5acadd19685002a589100f11e0680e5f4e746789
Manoj Karthick Selva Kumar <manojkarthick@ymail.com>
2020-12-05 23:56:27 -0800

    Ignore unused errors

--- src/errors.rs
+++ src/errors.rs
@@ -1,5 +1,6 @@
 use thiserror::Error;
 
+#[allow(dead_code)]
 #[derive(Error, Debug)]
 pub enum ReddSaverError {
     #[error("Missing environment variable")]
@@ -8,12 +9,16 @@ pub enum ReddSaverError {
     ReqwestError(#[from] reqwest::Error),
     #[error("Could not create directory")]
     CouldNotCreateDirectory,
-    #[error("Could not save image to filesystem")]
-    CouldNotSaveImageError,
-    #[error("Could not save image to filesystem")]
-    CouldNotCreateImageError,
+    #[error("Could not save image `{0}` to filesystem")]
+    CouldNotSaveImageError(String),
+    #[error("Could not create image `{0}` from `{1}`")]
+    CouldNotCreateImageError(String, String),
     #[error("Unable to join tasks")]
     TokioJoinError(#[from] tokio::task::JoinError),
     #[error("Could not save string to int")]
-    IntegerConversionError(#[from] std::num::ParseIntError),
+    ParsingIntError(#[from] std::num::ParseIntError),
+    #[error("Could not save usize to int")]
+    TryFromIntError(#[from] std::num::TryFromIntError),
+    #[error("Data directory not found, please check if it exists")]
+    DataDirNotFound,
 }
