d7a12673096d173ede945332ab4c14a1de6ee19c
Manoj Karthick <manojkarthick@ymail.com>
2021-02-17 03:55:34 -0800

    Add Media support for reddit-image/video/gif and gfycat/redgifs

--- src/download.rs
+++ src/download.rs
@@ -1,31 +1,51 @@
 use std::borrow::Borrow;
 use std::fs::File;
+use std::ops::Add;
 use std::path::Path;
 use std::sync::{Arc, Mutex};
 use std::{fs, io};
 
 use futures::stream::FuturesUnordered;
 use futures::TryStreamExt;
-use log::{debug, error, info, warn};
+use log::{debug, error, info};
+use url::{Position, Url};
 
 use crate::errors::ReddSaverError;
+use crate::structures::{GfyData, PostData};
 use crate::structures::{Summary, UserSaved};
 use crate::user::User;
 use crate::utils::check_path_present;
-use std::ops::Add;
+use reqwest::StatusCode;
 
-static URL_EXTENSION_JPG: &str = "jpg";
-static URL_EXTENSION_PNG: &str = "png";
-static URL_PREFIX_REDDIT_GALLERY: &str = "reddit.com/gallery";
-static URL_PREFIX_REDDIT_GALLERY_ITEM: &str = "https://i.redd.it";
+static JPG_EXTENSION: &str = "jpg";
+static PNG_EXTENSION: &str = "png";
+static GIF_EXTENSION: &str = "gif";
+// static GIFV_EXTENSION: &str = "gifv";
+static MP4_EXTENSION: &str = "mp4";
+
+static REDDIT_DOMAIN: &str = "reddit.com";
+static REDDIT_IMAGE_SUBDOMAIN: &str = "i.redd.it";
+static REDDIT_VIDEO_SUBDOMAIN: &str = "v.redd.it";
+static REDDIT_GALLERY_PATH: &str = "gallery";
+
+// static IMGUR_DOMAIN: &str = "imgur.com";
+// static IMGUR_SUBDOMAIN: &str = "i.imgur.com";
+
+static GFYCAT_DOMAIN: &str = "gfycat.com";
+static GFYCAT_API_PREFIX: &str = "https://api.gfycat.com/v1/gfycats";
+
+static REDGIFS_DOMAIN: &str = "redgifs.com";
+static REDGIFS_API_PREFIX: &str = "https://api.redgifs.com/v1/gfycats";
+
+// static GIPHY_DOMAIN: &str = "giphy.com";
 
 /// Status of image processing
-enum ImageStatus {
+enum MediaStatus {
     /// If we are able to successfully download the image
     Downloaded,
-    /// If we are skipping downloading the image due to it already being present
-    /// or because we could not find the image or because we are unable to decode
-    /// the image
+    /// If we are skipping downloading the media due to it already being present
+    /// or because we could not find the media or because we are unable to decode
+    /// the media
     Skipped,
 }
 
@@ -63,9 +83,9 @@ impl<'a> Downloader<'a> {
 
     pub async fn run(self) -> Result<(), ReddSaverError> {
         let mut full_summary = Summary {
-            images_downloaded: 0,
-            images_skipped: 0,
-            images_supported: 0,
+            media_downloaded: 0,
+            media_skipped: 0,
+            media_supported: 0,
         };
 
         for collection in self.saved {
@@ -75,26 +95,26 @@ impl<'a> Downloader<'a> {
         info!("#####################################");
         info!("Download Summary:");
         info!(
-            "Number of supported images: {}",
-            full_summary.images_supported
+            "Number of supported media: {}",
+            full_summary.media_supported
         );
         info!(
-            "Number of images downloaded: {}",
-            full_summary.images_downloaded
+            "Number of media downloaded: {}",
+            full_summary.media_downloaded
         );
-        info!("Number of images skipped: {}", full_summary.images_skipped);
+        info!("Number of media skipped: {}", full_summary.media_skipped);
         info!("#####################################");
         info!("FIN.");
 
         Ok(())
     }
 
-    /// Download and save images from Reddit in parallel
+    /// Download and save medias from Reddit in parallel
     async fn download_collection(&self, collection: &UserSaved) -> Result<Summary, ReddSaverError> {
         let summary = Arc::new(Mutex::new(Summary {
-            images_supported: 0,
-            images_downloaded: 0,
-            images_skipped: 0,
+            media_supported: 0,
+            media_downloaded: 0,
+            media_skipped: 0,
         }));
 
         collection
@@ -105,22 +125,12 @@ impl<'a> Downloader<'a> {
             // filter out the posts where a URL is present
             // not that this application cannot download URLs linked within the text of the post
             .filter(|item| item.data.url.is_some())
-            .filter(|item| {
-                let url_unwrapped = item.data.url.as_ref().unwrap();
-                // currently the supported image hosting sites (reddit, imgur) use an image extension
-                // at the end of the URLs. If the URLs end with jpg/png it is assumed to be an image
-                url_unwrapped.ends_with(URL_EXTENSION_JPG)
-                    || url_unwrapped.ends_with(URL_EXTENSION_PNG)
-                    || url_unwrapped.contains(URL_PREFIX_REDDIT_GALLERY)
-            })
             .map(|item| {
                 let summary_arc = summary.clone();
-                // since the latency for downloading an image from the network is unpredictable
-                // we spawn a new async task for the each of the images to be downloaded
+                // since the latency for downloading an media from the network is unpredictable
+                // we spawn a new async task for the each of the medias to be downloaded
                 async move {
-                    let url = item.data.url.borrow().as_ref().unwrap();
                     let subreddit = item.data.subreddit.borrow();
-                    let gallery_info = item.data.gallery_data.borrow();
                     let post_name = item.data.name.borrow();
                     let post_title = match item.data.title.as_ref() {
                         Some(t) => t,
@@ -140,62 +150,36 @@ impl<'a> Downloader<'a> {
                     if is_valid {
                         debug!("Subreddit VALID: {} present in {:#?}", subreddit, subreddit);
 
-                        // the set of images in this post is collected into this vector
-                        let mut image_info: Vec<(u32, String)> = Vec::new();
+                        let media = get_media(item.data.borrow()).await?;
+                        // every entry in this vector is valid media
+                        summary_arc.lock().unwrap().media_supported += media.len() as i32;
 
-                        // if the prefix is present in the URL we know that it's a reddit image gallery
-                        if url.contains(URL_PREFIX_REDDIT_GALLERY) {
-                            if let Some(gallery) = gallery_info {
-                                for (index, item) in gallery.items.iter().enumerate() {
-                                    // assemble the image URL from the media ID for the gallery item
-                                    let img_url = format!(
-                                        "{}/{}.{}",
-                                        URL_PREFIX_REDDIT_GALLERY_ITEM,
-                                        item.media_id,
-                                        URL_EXTENSION_JPG
-                                    );
-                                    debug!("Image URL from Gallery: {:#?}", img_url);
-                                    // push individual image URLs into the vector
-                                    image_info.push((index as u32, img_url));
-                                }
-                            } else {
-                                // empty galleries may be present when a user deletes the images present
-                                // in the gallery or the mods remove it
-                                warn!("Gallery at {} seems to be empty. Ignoring", url);
-                            }
-                        } else {
-                            // these URLs are for posts that directly contain a single image
-                            image_info.push((0, String::from(url)));
-                        }
-
-                        // every entry in this vector is a valid image
-                        summary_arc.lock().unwrap().images_supported += image_info.len() as i32;
-
-                        for (index, image_url) in &image_info {
+                        for (index, url) in media.iter().enumerate() {
                             let extension =
-                                String::from(image_url.split('.').last().unwrap_or("unknown"));
+                                String::from(url.split('.').last().unwrap_or("unknown"));
                             let file_name = self.generate_file_name(
-                                &image_url,
+                                &url,
                                 &subreddit,
                                 &extension,
                                 &post_name,
                                 &post_title,
                                 &index,
                             );
+
                             if self.should_download {
-                                let image_status = save_or_skip(image_url, &file_name);
+                                let status = save_or_skip(url, &file_name);
                                 // update the summary statistics based on the status
-                                match image_status.await? {
-                                    ImageStatus::Downloaded => {
-                                        summary_arc.lock().unwrap().images_downloaded += 1;
+                                match status.await? {
+                                    MediaStatus::Downloaded => {
+                                        summary_arc.lock().unwrap().media_downloaded += 1;
                                     }
-                                    ImageStatus::Skipped => {
-                                        summary_arc.lock().unwrap().images_skipped += 1
+                                    MediaStatus::Skipped => {
+                                        summary_arc.lock().unwrap().media_skipped += 1
                                     }
                                 }
                             } else {
-                                info!("Image available at URL: {}", &image_url);
-                                summary_arc.lock().unwrap().images_skipped += 1;
+                                info!("Media available at URL: {}", &url);
+                                summary_arc.lock().unwrap().media_skipped += 1;
                             }
                         }
                     } else {
@@ -220,14 +204,14 @@ impl<'a> Downloader<'a> {
 
         debug!("Collection statistics: ");
         debug!(
-            "Number of supported images: {}",
-            local_summary.images_supported
+            "Number of supported media: {}",
+            local_summary.media_supported
         );
         debug!(
-            "Number of images downloaded: {}",
-            local_summary.images_downloaded
+            "Number of media downloaded: {}",
+            local_summary.media_downloaded
         );
-        debug!("Number of images skipped: {}", local_summary.images_skipped);
+        debug!("Number of media skipped: {}", local_summary.media_skipped);
 
         Ok(local_summary)
     }
@@ -240,15 +224,16 @@ impl<'a> Downloader<'a> {
         extension: &str,
         name: &str,
         title: &str,
-        index: &u32,
+        index: &usize,
     ) -> String {
         return if !self.use_human_readable {
-            // create a hash for the image using the URL the image is located at
-            // this helps to make sure the image download always writes the same file
+            // create a hash for the media using the URL the media is located at
+            // this helps to make sure the media download always writes the same file
             // name irrespective of how many times it's run. If run more than once, the
-            // image is overwritten by this method
+            // media is overwritten by this method
             let hash = md5::compute(url);
             format!(
+                // TODO: Fixme, use appropriate prefix
                 "{}/{}/img-{:x}.{}",
                 self.data_directory, subreddit, hash, extension
             )
@@ -285,23 +270,23 @@ impl<'a> Downloader<'a> {
     }
 }
 
-/// Helper function that downloads and saves a single image from Reddit or Imgur
-async fn save_or_skip(url: &str, file_name: &str) -> Result<ImageStatus, ReddSaverError> {
+/// Helper function that downloads and saves a single media from Reddit or Imgur
+async fn save_or_skip(url: &str, file_name: &str) -> Result<MediaStatus, ReddSaverError> {
     if check_path_present(&file_name) {
         debug!("Image from url {} already downloaded. Skipping...", url);
-        Ok(ImageStatus::Skipped)
+        Ok(MediaStatus::Skipped)
     } else {
-        let save_status = download_image(&file_name, &url).await?;
+        let save_status = download_media(&file_name, &url).await?;
         if save_status {
-            Ok(ImageStatus::Downloaded)
+            Ok(MediaStatus::Downloaded)
         } else {
-            Ok(ImageStatus::Skipped)
+            Ok(MediaStatus::Skipped)
         }
     }
 }
 
-/// Download image from the given url and save to data directory. Also create data directory if not present already
-async fn download_image(file_name: &str, url: &str) -> Result<bool, ReddSaverError> {
+/// Download media from the given url and save to data directory. Also create data directory if not present already
+async fn download_media(file_name: &str, url: &str) -> Result<bool, ReddSaverError> {
     // create directory if it does not already exist
     // the directory is created relative to the current working directory
     let directory = Path::new(file_name).parent().unwrap();
@@ -313,12 +298,105 @@ async fn download_image(file_name: &str, url: &str) -> Result<bool, ReddSaverErr
     let data = reqwest::get(url).await?.bytes().await?;
     let mut output = File::create(&file_name)?;
     match io::copy(&mut data.as_ref(), &mut output) {
-        Ok(_) => info!("Successfully saved image: {} from url {}", file_name, url),
+        Ok(_) => info!("Successfully saved media: {} from url {}", file_name, url),
         Err(_e) => {
-            error!("Could not save image from url {} to {}", url, file_name);
+            error!("Could not save media from url {} to {}", url, file_name);
             return Ok(false);
         }
     }
 
     Ok(true)
+}
+
+async fn gfy_to_mp4(url: &str) -> Result<Option<String>, ReddSaverError> {
+    let api_prefix = if url.contains(GFYCAT_DOMAIN) {
+        GFYCAT_API_PREFIX
+    } else {
+        REDGIFS_API_PREFIX
+    };
+    let maybe_media_id = url.split("/").last();
+
+    if let Some(media_id) = maybe_media_id {
+        let api_url = format!("{}/{}", api_prefix, media_id);
+        debug!("GFY API URL: {}", api_url);
+        let client = reqwest::Client::new();
+
+        let response = client.get(&api_url).send().await?;
+        if response.status() == StatusCode::OK {
+            let data = response.json::<GfyData>().await?;
+            Ok(Some(data.gfy_item.mp4_url))
+        } else {
+            Ok(None)
+        }
+    } else {
+        Ok(None)
+    }
+}
+
+/// Check if a particular URL contains supported media.
+async fn get_media(data: &PostData) -> Result<Vec<String>, ReddSaverError> {
+    let original = data.url.as_ref().unwrap();
+    let mut parsed = Url::parse(original)?;
+    parsed.path_segments_mut().unwrap().pop_if_empty();
+    let url = &parsed[..Position::AfterPath];
+    let gallery_info = data.gallery_data.borrow();
+    let mut media: Vec<String> = Vec::new();
+
+    // reddit images and gifs
+    if url.contains(REDDIT_IMAGE_SUBDOMAIN) {
+        if url.ends_with(JPG_EXTENSION)
+            || url.ends_with(PNG_EXTENSION)
+            || url.ends_with(GIF_EXTENSION)
+        {
+            let translated = String::from(url);
+            media.push(translated);
+        }
+    }
+
+    // reddit mp4 videos
+    if url.contains(REDDIT_VIDEO_SUBDOMAIN) {
+        if url.ends_with(MP4_EXTENSION) {
+            let translated = String::from(url);
+            media.push(translated);
+        } else {
+            if let Some(m) = &data.media {
+                if let Some(v) = &m.reddit_video {
+                    let translated = String::from(&v.fallback_url).replace("?source=fallback", "");
+                    media.push(translated);
+                }
+            }
+        }
+    }
+
+    // reddit image galleries
+    if url.contains(REDDIT_DOMAIN) && url.contains(REDDIT_GALLERY_PATH) {
+        if let Some(gallery) = gallery_info {
+            for item in gallery.items.iter() {
+                // TODO: fixme; use better way to create this URL
+                let translated = format!(
+                    "https://{}/{}.{}",
+                    REDDIT_IMAGE_SUBDOMAIN, item.media_id, JPG_EXTENSION
+                );
+                media.push(translated);
+            }
+        }
+    }
+
+    // gfycat and redgifs
+    if url.contains(GFYCAT_DOMAIN) || url.contains(REDGIFS_DOMAIN) {
+        if url.ends_with(MP4_EXTENSION) {
+            let translated = String::from(url);
+            media.push(translated);
+        } else {
+            if let Some(mp4_url) = gfy_to_mp4(url).await? {
+                media.push(mp4_url);
+            }
+        }
+    }
+
+    // TODO:
+    // Imgur - single images, image post, image gallery, imgur gifv
+    // Giphy domain - gifs
+
+    Ok(media)
 }
